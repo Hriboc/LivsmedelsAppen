3fdd0fbe127b184af2f7d5976ece4c59c1fc9735
Hrisitijan <hristijan.bocevski@gmail.com>
2017-02-22 11:04:11 +0100

    Initial Commit

---
+++ LivsmedelsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivsmedelsApp
+//
+//  Created by Hristijan Bocevski on 2017-02-22.
+//  Copyright © 2017 Hristijan Bocevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
